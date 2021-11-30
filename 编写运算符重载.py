0e094b7575b3cf93fd81d26b815155b30ec92464
MengDehao <834086677@qq.com>
2021-11-30 21:11:35 +0800

    改错

--- 编写运算符重载.py
+++ 编写运算符重载.py
@@ -6,7 +6,7 @@
 '''说明:参照例9.23，编写运算符重载的程序
 '''
 class Mylist:
-    def __init__(self,*args):
+    def __init__(self, * args):
         self.__mylist = []
         for arg in args:
             self.__mylist.append(arg)
@@ -28,19 +28,19 @@ class Mylist:
             self.__mylist[i] /= n
 
     def __len__(self):
-        return (len(self.__mylist))
+        return len(self.__mylist)
 
     def __repr__(self):
         str1 = ''
         for i in range(0,len(self.__mylist)):
-            str1 += str(self.__mylist) + ' '
+            str1 += str(self.__mylist[i]) + ' '
         return str1
 
 
 if __name__ == "__main__":
     m = Mylist(1,2,3,4,5)
     m + 2;print(repr(m))
-    m - 2;print(repr(m))
-    m * 2;print(repr(m))
+    m - 1;print(repr(m))
+    m * 4;print(repr(m))
     m / 2;print(repr(m))
     print(len(m))
